e1b0bf2c4478ffd378b1f62a817f4fcbe3fcf5d8
Dev <124387485+imKingDevs@users.noreply.github.com>
2023-03-08 17:03:59 +0530

    Commited

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+ReactDOM.render(
+    [
+        <h1>I am Parth!</h1>,
+        <p>Parth is a devloper.</p>
+    ],
+    document.querySelector('#root')); 
